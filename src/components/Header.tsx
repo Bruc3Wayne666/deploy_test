373de934fbb435cf57db6701a9fbb93c78b26450
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-10-23 21:20:18 +0300

    k

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {Link} from 'react-router-dom';
-import {Slide} from 'react-slideshow-image';
+// import {Slide} from 'react-slideshow-image';
 import '../index.css'
 
 // const SimpleSlider: FC<any> = () => {
