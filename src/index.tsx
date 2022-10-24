3ec9cca51e89761703d6a079cd0635719c514061
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-10-24 22:43:32 +0300

    -_-

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App'
 import { setupStore } from './store/store'
 import { Provider } from 'react-redux'
-import {BrowserRouter, HashRouter} from "react-router-dom";
-// import { BrowserRouter } from 'react'
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const store = setupStore()
 root.render(
 	<Provider store={store}>
-			<BrowserRouter basename='/deploy_test'>
+			{/*<BrowserRouter basename='/deploy_test'>*/}
+			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 	</Provider>
