57f9f31ea29bc9a92819d7ea1df957ede85c9cdd
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-11-12 01:51:54 +0300

    ??

--- src/components/Cybers.tsx
+++ src/components/Cybers.tsx
@@ -2,7 +2,7 @@ import React, {FC} from 'react';
 
 const CyberSport:FC = () => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{marginTop: 300, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <h1>SOON</h1>
         </div>
     );
